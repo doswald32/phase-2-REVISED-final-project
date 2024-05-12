ec944e984d80aa883b88e270eb459b52b5950665
doswald32 <dan.oswald32@gmail.com>
2024-05-12 16:47:20 -0600

    added link to each brand in brands which directs to shoes component

--- dans-phase-2-final-project/src/Brands.js
+++ dans-phase-2-final-project/src/Brands.js
@@ -1,4 +1,5 @@
 import NavBar from "./NavBar";
+import { Link } from "react-router-dom";
 
 function Brands() {
     return (
@@ -11,17 +12,20 @@ function Brands() {
                     <div className="brand">
                         <h2>Nike</h2>
                         <img className="brand-image" src="https://images.footlocker.com/content/dam/Resources/shared/brand-logos/nike/nike.svg" alt="Nike logo" />
-                        <p id="nike-shoes">Click to see Nike shoes</p>
+                        <br/>
+                        <Link to="/shoes">Click to see Nike shoes</Link>
                     </div>
                     <div className="brand">
                         <h2>Jordan</h2>
                         <img className="brand-image" src="https://images.footlocker.com/content/dam/Resources/shared/brand-logos/jordan.svg" alt="Jordan logo" />
-                        <p id="jordan-shoes">Click to see Jordan shoes</p>
+                        <br/>
+                        <Link to="/shoes">Click to see Jordan shoes</Link>
                     </div>
                     <div className="brand">
                         <h2>Adidas</h2>
                         <img className="brand-image" src="https://images.footlocker.com/content/dam/Resources/shared/brand-logos/adidas/adidas-originals.svg" alt="Adidas logo" />
-                        <p id="adidas-shoes">Click to see Adidas shoes</p>
+                        <br/>
+                        <Link to="/shoes">Click to see Adidas shoes</Link>
                     </div>
                 </div>
                 
