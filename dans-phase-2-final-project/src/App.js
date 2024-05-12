52b2b556886320c2d568721f6b9434f1c585fe7c
doswald32 <dan.oswald32@gmail.com>
2024-05-12 14:56:47 -0600

    updated app.js

--- dans-phase-2-final-project/src/App.js
+++ dans-phase-2-final-project/src/App.js
@@ -4,10 +4,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <NavBar />
       </header>
       <main>
-        
+        <h1>Welcome!</h1>
       </main>
     </div>
   );
