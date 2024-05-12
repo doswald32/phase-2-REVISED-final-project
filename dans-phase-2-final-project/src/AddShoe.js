9e60909f7b8261bb37ff6e9a40e6d5a76ffe94a8
doswald32 <dan.oswald32@gmail.com>
2024-05-12 16:24:48 -0600

    added form to add a shoe component

--- dans-phase-2-final-project/src/AddShoe.js
+++ dans-phase-2-final-project/src/AddShoe.js
@@ -1,13 +1,43 @@
 import NavBar from "./NavBar";
+import React, { useState } from "react";
 
 function AddShoe() {
+    const [newShoe, setNewShoe] = useState({
+        brand: "",
+        name: "",
+        price: "",
+        imageURL: ""
+    });
+
     return (
         <>
             <header>
                 <NavBar />
             </header>
             <main>
-                <div>Add a Shoe</div>
+            <form className="form">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td align="right">Brand: </td>
+                                <td align="left"><input id="brand" type="text" value={newShoe.brand}/></td>
+                            </tr>
+                            <tr>
+                                <td align="right">Name: </td>
+                                <td align="left"><input id="name" type="text" value={newShoe.name}/></td>
+                            </tr>
+                            <tr>
+                                <td align="right">Price: </td>
+                                <td align="left"><input id="price" type="text" value={newShoe.price}/></td>
+                            </tr>
+                            <tr>
+                                <td align="right">Image URL: </td>
+                                <td align="left"><input id="imageURL" type="text" value={newShoe.imageURL}/></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button className="button">Submit</button>
+                </form>
             </main>
         </>
     )
