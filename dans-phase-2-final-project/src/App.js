ccac4e6723926d8e8b18187e6437384fd4314f04
doswald32 <dan.oswald32@gmail.com>
2024-05-12 14:59:59 -0600

    updated index.js file with routes

--- dans-phase-2-final-project/src/App.js
+++ dans-phase-2-final-project/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import NavBar from "./NavBar";
 
 function App() {
   return (
--- dans-phase-2-final-project/src/index.js
+++ dans-phase-2-final-project/src/index.js
@@ -3,13 +3,38 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
