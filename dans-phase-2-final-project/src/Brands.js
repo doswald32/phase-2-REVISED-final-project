d13eac21faa18bc19b93e95e7ebdb1e5db50743c
doswald32 <dan.oswald32@gmail.com>
2024-05-12 16:18:55 -0600

    added logos and other context to brands component

--- dans-phase-2-final-project/src/Brands.js
+++ dans-phase-2-final-project/src/Brands.js
@@ -7,7 +7,24 @@ function Brands() {
                 <NavBar />
             </header>
             <main>
-                <div>Brands</div>
+                <div className="brands-container">
+                    <div className="brand">
+                        <h2>Nike</h2>
+                        <img className="brand-image" src="https://images.footlocker.com/content/dam/Resources/shared/brand-logos/nike/nike.svg" alt="Nike logo" />
+                        <p id="nike-shoes">Click to see Nike shoes</p>
+                    </div>
+                    <div className="brand">
+                        <h2>Jordan</h2>
+                        <img className="brand-image" src="https://images.footlocker.com/content/dam/Resources/shared/brand-logos/jordan.svg" alt="Jordan logo" />
+                        <p id="jordan-shoes">Click to see Jordan shoes</p>
+                    </div>
+                    <div className="brand">
+                        <h2>Adidas</h2>
+                        <img className="brand-image" src="https://images.footlocker.com/content/dam/Resources/shared/brand-logos/adidas/adidas-originals.svg" alt="Adidas logo" />
+                        <p id="adidas-shoes">Click to see Adidas shoes</p>
+                    </div>
+                </div>
+                
             </main>
         </>
     )
